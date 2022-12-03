a5c921942236d4cd76bf1fcd9cc6d9624a51adf6
Genesis Pinto <genesisgps@gmail.com>
2022-12-03 12:40:56 -0300

    create Listview

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+
+  final opcions = const ['Hamburguesas', 'Tequeños',' Sushi', 'Sopas'];
+   
+  const Listview2Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview tipo 2'),
+      ),
+      body: ListView(
+        children: [
+          ...opcions.map((e) => ListTile(
+            leading: const Icon(Icons.ac_unit_outlined),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            title:   Text(e),
+           
+          )
+          ).toList(),
+           const Divider()
+
+         
+        
+        ],
+      )
+    );
+  }
+} 
