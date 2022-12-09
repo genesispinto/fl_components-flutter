df6f5e4e5de0cbbdd35b1af836dbd4b47defd8a4
Genesis Pinto <genesisgps@gmail.com>
2022-12-09 12:22:13 -0300

    Animated Container

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -11,17 +11,18 @@ class AppRoutes {
   static const initialRoute = 'home';
   // Listado de opciones con la ruta definida
   static final menuOption = <MenuOption>[
-    MenuOption(route: 'home',  name:'Home Screen', screen: const HomeScreen(), icon: Icons.home),
+    //MenuOption(route: 'home',  name:'Home Screen', screen: const HomeScreen(), icon: Icons.home),
     MenuOption(route: 'listview1',  name:'listview tipo 1', screen: const Listview1Screen(), icon: Icons.light_mode_sharp),
     MenuOption(route: 'listview2',  name:'listview tipo 2', screen: const Listview2Screen(), icon: Icons.monetization_on),
     MenuOption(route: 'alert',  name:'Alert Screen', screen: const AlertScreen(), icon: Icons.sim_card_alert_outlined),
     MenuOption(route: 'card',  name:'Card Screen', screen: const CardScreen(), icon: Icons.card_giftcard),
-    MenuOption(route: 'avatar',  name:'Circle Avatar ', screen: const AvatarScreen(), icon: Icons.person)
+    MenuOption(route: 'avatar',  name:'Circle Avatar ', screen: const AvatarScreen(), icon: Icons.person),
+    MenuOption(route: 'animated',  name:'Animated Container ', screen: const AnimatedScreen(), icon: Icons.play_circle_fill_outlined)
   ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes(){
       Map<String, Widget Function(BuildContext)> appRoutes ={};
-
+          appRoutes.addAll({'home':(context) => const HomeScreen()});
         for(final option in menuOption){
           appRoutes.addAll({option.route:(context) => option.screen});
         }
